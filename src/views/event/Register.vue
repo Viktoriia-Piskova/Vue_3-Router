8602c55aea6e8a95f32744fc6ef59783cbda4012
Viktoriia-Piskova <viktoriapiskova@gmail.com>
2022-11-14 15:36:49 +0200

    Programmatic navigation

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,11 +1,18 @@
 <template>
   <div v-if="event">
     <p>Register for event here</p>
+    <button @click="register">Register me</button>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  methods: {
+    register() {
+      //API call. If success then redirect to event details
+      this.$router.push({ name: 'EventDetails', params: { id: this.event.id } })
+    }
+  }
 }
 </script>
