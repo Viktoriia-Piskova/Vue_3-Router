d307bc5cb16b697cbf91c84149d5c6a670b9c3ec
Viktoriia-Piskova <viktoriapiskova@gmail.com>
2022-11-15 18:42:48 +0200

    beforeRouterLeave

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -6,6 +6,25 @@
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  data() {
+    return {
+      unsavedChanges: true
+    }
+  },
+  beforeRouteLeave(routeTo, routeFrom, next) {
+    if (this.unsavedChanges) {
+      const answer = window.confirm(
+        'Do you really want to leave? You have unsaved changes'
+      )
+      if (answer) {
+        next()
+      } else {
+        next(false)
+      }
+    } else {
+      next()
+    }
+  }
 }
 </script>
