0752c75310557400ced4b354c4a3e80e3c494435
Viktoriia-Piskova <viktoriapiskova@gmail.com>
2022-11-16 12:51:15 +0200

    Loader NProgress

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -24,7 +24,7 @@
 <script>
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
-import { watchEffect } from 'vue'
+import NProgress from 'nprogress'
 
 export default {
   name: 'EventList',
@@ -38,29 +38,40 @@ export default {
       totalEvents: 0
     }
   },
-  created() {
-    watchEffect(() => {
-      this.events = null
-      EventService.getEvents(2, this.page)
-        .then(response => {
-          this.events = response.data
-          this.totalEvents = response.headers['x-total-count']
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    NProgress.start()
+    EventService.getEvents(2, parseInt(routeTo.query.page) || 1)
+      .then(response => {
+        next(comp => {
+          comp.events = response.data
+          comp.totalEvents = response.headers['x-total-count']
         })
-        .catch(error => {
-          if (error.response && error.response.status == 404) {
-            this.$router.push({
-              name: '404Resource',
-              params: { resource: 'event' }
-            })
-          } else {
-            this.$router.push({ name: 'NetworkError' })
-          }
-        })
-    })
+      })
+      .catch(() => {
+        next({ name: 'NetworkError' })
+      })
+      .finally(() => {
+        NProgress.done()
+      })
   },
+  beforeRouteUpdate(routeTo) {
+    NProgress.start()
+    EventService.getEvents(2, parseInt(routeTo.query.page) || 1)
+      .then(response => {
+        this.events = response.data
+        this.totalEvents = response.headers['x-total-count']
+      })
+      .catch(() => {
+        return { name: 'NetworkError' }
+      })
+      .finally(() => {
+        NProgress.done()
+      })
+  },
+
   computed: {
     hasNextPage() {
-      const totalPages = Math.ceil(this.totalEvents / 2)
+      var totalPages = Math.ceil(this.totalEvents / 2)
       return this.page < totalPages
     }
   }
